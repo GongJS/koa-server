50ccde7c3cb5e92f5060a1dd8265ef51546e33f9
GongJS <382663074@qq.com>
2019-08-31 20:56:45 +0800

    deploy

--- index.js
+++ index.js
@@ -3,30 +3,40 @@ const app = new Koa();
 const path = require('path');
 const koaStatic = require('koa-static')
 const koaBody = require('koa-body')
+const gm = require('gm')
 const cors = require('koa2-cors');
 const PORT = process.env.PORT || 3000
 
-app.use(cors({origin: 'https://gongjs.github.io'}))
+app.use(cors({ origin: '*' }))
 app.use(koaStatic(path.join(__dirname, 'public')))
 app.use(koaBody({
-  multipart:true,
+  multipart: true,
   formidable: {
-    uploadDir: path.join(__dirname,'/public/uploads'),
-    keepExtensions:true
-  }
+    uploadDir: path.join(__dirname, '/public/uploads'),
+    keepExtensions: true,
+  },
 }))
 app.use(async (ctx, next) => {
-    if (ctx.request.path === '/upload') {
-      const file = ctx.request.files.file
-      const basename = path.basename(file.path)
-      ctx.body= {
-        url: `${ctx.origin}/uploads/${basename}`
-      }
-    } else {
-        await next();
+  if (ctx.request.path === '/upload') {
+    const file = ctx.request.files.file
+    const basename = path.basename(file.path)
+    gm(`./public/uploads/${basename}`)
+      .size(function (err, size) {
+        if (!err) {
+          console.log('width = ' + size.width);
+          console.log('height = ' + size.height);
+        }
+      });
+    ctx.body = {
+      url: `${ctx.origin}/uploads/${basename}`,
+      width: size.width,
+      height: size.height
     }
+  } else {
+    await next();
+  }
 });
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
   console.log('server is running at http://localhost:3000')
 })
