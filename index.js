0831c73565674608c3c40d2453588daaa15f98a5
GongJS <382663074@qq.com>
2019-10-01 16:33:27 +0800

    fix

--- index.js
+++ index.js
@@ -4,10 +4,22 @@ const path = require('path');
 const koaStatic = require('koa-static')
 const koaBody = require('koa-body')
 const gm = require('gm')
-const cors = require('koa2-cors');
+const cors = require('@koa/cors');
 const PORT = process.env.PORT || 3000
 
-app.use(cors({ origin: '*' }))
+const hanldeImage = (filePath) => {
+  return new Promise((resolve, reject) => {
+    gm(filePath).size((err,size) => {
+      if (!err) {
+        resolve({width:size.width, height:size.height})
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
+
+app.use(cors())
 app.use(koaStatic(path.join(__dirname, 'public')))
 app.use(koaBody({
   multipart: true,
@@ -17,20 +29,16 @@ app.use(koaBody({
   },
 }))
 app.use(async (ctx, next) => {
-  if (ctx.request.path === '/upload') {
-    const file = ctx.request.files.file
+  if (ctx.request.url === '/upload') {
+    const file = ctx.request.files.post
     const basename = path.basename(file.path)
-    gm(`./public/uploads/${basename}`)
-      .size(function (err, size) {
-        if (!err) {
-          console.log('width = ' + size.width);
-          console.log('height = ' + size.height);
-        }
-      });
+    const imageRes = await hanldeImage(`public/uploads/${basename}`)
+    const height = imageRes.height
+    const width = imageRes.width
     ctx.body = {
       url: `${ctx.origin}/uploads/${basename}`,
-      width: size.width,
-      height: size.height
+      width,
+      height
     }
   } else {
     await next();
